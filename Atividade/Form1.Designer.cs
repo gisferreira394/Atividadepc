4b6a1c55fcc3c10e04b5499ff1449e5b8d7062ac
Gi Silva <112526312+gisferreira394@users.noreply.github.com>
2023-03-03 21:18:38 -0300

    C# pronto vdd ou falso

--- Atividade/Form1.Designer.cs
+++ Atividade/Form1.Designer.cs
@@ -45,6 +45,13 @@
             this.button2 = new System.Windows.Forms.Button();
             this.radioButton7 = new System.Windows.Forms.RadioButton();
             this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -64,6 +71,8 @@
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // pictureBox2
             // 
@@ -74,6 +83,7 @@
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
             // 
             // pictureBox3
             // 
@@ -84,6 +94,7 @@
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 2;
             this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
             // 
             // pictureBox4
             // 
@@ -94,6 +105,7 @@
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox4.TabIndex = 3;
             this.pictureBox4.TabStop = false;
+            this.pictureBox4.Visible = false;
             // 
             // pictureBox5
             // 
@@ -104,6 +116,7 @@
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox5.TabIndex = 4;
             this.pictureBox5.TabStop = false;
+            this.pictureBox5.Visible = false;
             // 
             // pictureBox6
             // 
@@ -114,6 +127,7 @@
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox6.TabIndex = 5;
             this.pictureBox6.TabStop = false;
+            this.pictureBox6.Visible = false;
             // 
             // radioButton1
             // 
@@ -125,6 +139,7 @@
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Notebook1";
             this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
             // radioButton2
             // 
@@ -136,6 +151,7 @@
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Notebook2";
             this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             // 
             // radioButton3
             // 
@@ -147,6 +163,7 @@
             this.radioButton3.TabStop = true;
             this.radioButton3.Text = "Notebook3";
             this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
             // 
             // radioButton4
             // 
@@ -158,6 +175,7 @@
             this.radioButton4.TabStop = true;
             this.radioButton4.Text = "Computador1";
             this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
             // 
             // radioButton5
             // 
@@ -169,6 +187,7 @@
             this.radioButton5.TabStop = true;
             this.radioButton5.Text = "Computador2";
             this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
             // 
             // radioButton6
             // 
@@ -180,24 +199,27 @@
             this.radioButton6.TabStop = true;
             this.radioButton6.Text = "Computador3";
             this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(465, 408);
+            this.button1.Location = new System.Drawing.Point(572, 408);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(93, 30);
             this.button1.TabIndex = 12;
             this.button1.Text = "Limpar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(678, 408);
+            this.button2.Location = new System.Drawing.Point(695, 408);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(93, 30);
             this.button2.TabIndex = 13;
             this.button2.Text = "Sair";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // radioButton7
             // 
@@ -209,6 +231,7 @@
             this.radioButton7.TabStop = true;
             this.radioButton7.Text = "Computador";
             this.radioButton7.UseVisualStyleBackColor = true;
+            this.radioButton7.CheckedChanged += new System.EventHandler(this.radioButton7_CheckedChanged);
             // 
             // radioButton8
             // 
@@ -222,11 +245,89 @@
             this.radioButton8.UseVisualStyleBackColor = true;
             this.radioButton8.CheckedChanged += new System.EventHandler(this.radioButton8_CheckedChanged);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(234, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "R$ 3.000,00";
+            this.label1.Visible = false;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(234, 256);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "R$ 10.000,00";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(234, 406);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "R$4.500,00";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(615, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "R$6.500,00";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(615, 244);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "R$2.500,00";
+            this.label5.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(615, 371);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "R$1.500,00";
+            this.label6.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(454, 408);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 30);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "Selecionar tudo";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.radioButton8);
             this.Controls.Add(this.radioButton7);
             this.Controls.Add(this.button2);
@@ -274,6 +375,13 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.RadioButton radioButton7;
         private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Atividade/Form1.cs
+++ Atividade/Form1.cs
@@ -19,7 +19,208 @@ namespace Atividade
 
         private void radioButton8_CheckedChanged(object sender, EventArgs e)
         {
-            this
+            this.pictureBox1.Visible = true;
+            this.pictureBox2.Visible = true;
+            this.pictureBox3.Visible = true;
+
+            this.label1.Visible = true;
+            this.label2.Visible = true;
+            this.label3.Visible = true;
+           
+            this.pictureBox4.Visible = false;
+            this.pictureBox5.Visible = false;
+            this.pictureBox6.Visible = false;
+
+            this.label4.Visible = false;
+            this.label5.Visible = false;
+            this.label6.Visible = false;
+        }
+
+        private void radioButton7_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureBox4.Visible= true;
+                this.pictureBox5.Visible= true;
+                this.pictureBox6.Visible= true;
+
+            this.label4.Visible = true;
+            this.label5.Visible = true;
+            this.label6.Visible = true;
+
+            this.pictureBox1.Visible = false;
+this.pictureBox2.Visible= false;
+            this.pictureBox3.Visible= false;
+
+            this.label1.Visible = false;
+            this.label2.Visible = false;
+            this.label3.Visible = false;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureBox3.Visible = true;
+
+            this.label3.Visible = true;
+
+            this.label2.Visible = false;
+            this.label1.Visible = false;
+            this.label4.Visible = false;
+            this.label5.Visible = false;
+            this.label6.Visible = false;
+
+            this.pictureBox2.Visible = false;
+            this.pictureBox5.Visible = false; 
+            this.pictureBox4.Visible = false;
+            this.pictureBox1.Visible = false;
+            this.pictureBox6.Visible = false;
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureBox4.Visible = true;
+            this.label4.Visible = true;
+
+            this.label2.Visible = false;
+            this.label3.Visible = false;
+            this.label1.Visible = false;
+            this.label5.Visible = false;
+            this.label6.Visible = false;
+
+            this.pictureBox5.Visible = false; 
+            this.pictureBox1.Visible = false;
+            this.pictureBox2.Visible = false;
+            this.pictureBox3.Visible = false;
+            this.pictureBox6.Visible = false;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureBox1.Visible= true;
+            this.label1.Visible= true;
+
+            this.label2.Visible = false;
+            this.label3.Visible = false;
+            this.label4.Visible = false;
+            this.label5.Visible = false;
+            this.label6.Visible = false;
+
+            this.pictureBox2.Visible = false;
+            this.pictureBox3.Visible = false;
+            this.pictureBox4.Visible = false;
+            this.pictureBox5.Visible= false;
+            this.pictureBox6.Visible = false;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureBox2.Visible = true;
+            this.label2.Visible = true;
+
+            this.label1.Visible = false;
+            this.label3.Visible = false;
+            this.label4.Visible = false;
+            this.label5.Visible = false;
+            this.label6.Visible = false;
+
+            this.pictureBox1.Visible = false;
+            this.pictureBox3.Visible= false;    
+            this.pictureBox4.Visible= false;
+            this.pictureBox5.Visible= false;
+            this.pictureBox6.Visible= false;
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureBox5.Visible = true;
+            this.label5.Visible = true;
+
+            this.label2.Visible = false;
+            this.label3.Visible = false;
+            this.label4.Visible = false;
+            this.label1.Visible = false;
+            this.label6.Visible = false;
+
+            this.pictureBox4.Visible= false;
+            this.pictureBox1.Visible = false;
+            this.pictureBox2.Visible=false;
+            this.pictureBox3.Visible=false;
+            this.pictureBox6.Visible = false;
+
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureBox6.Visible = true;
+            this.label6.Visible = true;
+
+            this.label2.Visible = false;
+            this.label3.Visible = false;
+            this.label4.Visible = false;
+            this.label5.Visible = false;
+            this.label1.Visible = false;
+
+            this.pictureBox5.Visible = false;
+            this.pictureBox4.Visible = false;
+            this.pictureBox1.Visible = false;
+            this.pictureBox2.Visible = false;
+            this.pictureBox3.Visible = false;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.pictureBox1.Visible = false;
+            this.pictureBox2.Visible = false;
+            this.pictureBox3.Visible = false;
+            this.pictureBox4.Visible = false;
+            this.pictureBox5.Visible = false;
+            this.pictureBox6.Visible = false;
+            this.label1.Visible = false;
+            this.label2.Visible = false;
+            this.label3.Visible = false;
+            this.label4.Visible = false;
+            this.label5.Visible = false;
+            this.label6.Visible = false;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           Close();
+              
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.pictureBox1.Visible = true;
+            this.pictureBox2.Visible = true;
+            this.pictureBox3.Visible = true;
+            this.pictureBox4.Visible = true;
+            this.pictureBox5.Visible = true;
+            this.pictureBox6.Visible = true;
+            this.label1.Visible = true;
+            this.label2.Visible = true;
+            this.label3.Visible = true;
+            this.label4.Visible = true;
+            this.label5.Visible = true;
+            this.label6.Visible = true;
+            this.radioButton1.Visible = true;
+            this.radioButton2.Visible = true;
+            this.radioButton3.Visible = true;
+            this.radioButton4.Visible = true;
+            this.radioButton5.Visible = true;
+            this.radioButton6.Visible = true;
+            this.radioButton7.Visible = true;
+            this.radioButton8.Visible = true;
         }
     }
 }
